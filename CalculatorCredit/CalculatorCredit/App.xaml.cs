ee25305cf46f3dd99921a5536dd516420aa83591
yuliqywer <vikatrefilova8484@gmail.com>
2026-06-15 03:35:33 +0500

    первый коммит

---
+++ CalculatorCredit/CalculatorCredit/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorCredit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new WelcomePage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ CalculatorCredit/CalculatorCredit/WelcomePage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CalculatorCredit
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomePage : ContentPage
+    {
+        public WelcomePage()
+        {
+            InitializeComponent();
+        }
+        private async void SignInClicked(object sender, EventArgs e)
+        {
+            // проверка на заполнение полей
+            if (string.IsNullOrWhiteSpace(SurnameEntry.Text))
+            {
+                await DisplayAlert("Ошибка", "Пожалуйста, введите вашу Фамилию (Username)", "ОК");
+                return;
+            }
+
+            // переход на второй экран с передачей фамилии пользователя
+            string userSurname = SurnameEntry.Text.Trim();
+            await Navigation.PushAsync(new CalculatorPage(userSurname));
+        }
+    }
+}
